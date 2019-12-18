765c5d2c843869c7ceed02590afc9dd9c9f4a6e5
realsdx <realsdx@protonmail.com>
2019-12-18 16:51:13 +0530

    update pre process file

--- sentiment_analysis/pre_process_data.sh
+++ sentiment_analysis/pre_process_data.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-python ./sentiment_analysis/donwload.py
+python ./sentiment_analysis/download.py
 python ./sentiment_analysis/verify.py
 
 mv /root/.cache/paddle/dataset/imdb/aclImdb_v1.tar.gz /root/.cache/paddle/dataset/imdb/imdb%2FaclImdb_v1.tar.gz
